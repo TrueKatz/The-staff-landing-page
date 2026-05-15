7486c305d6f827ce20b6696e428b370332a83444
Engineer0808 <negociosbordinhao@gmail.com>
2026-05-15 19:21:54 -0300

    Mantem previa de link em portugues

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const RATE_REFRESH_MS = 5 * 60 * 1000;
 const BOT_TELEGRAM_URL = "https://t.me/WardenControlBot";
 const CONTACT_TELEGRAM_URL = "https://t.me/TrueKatz666";
 const META_PIXEL_FALLBACK_ID = "";
-const SITE_BASE_URL = "https://truekatz.github.io/The-staff-landing-page/";
 
 const previewImages = {
   "pt-BR": {
@@ -745,13 +744,8 @@ function getPreviewImage(language) {
   return previewImages[language] || previewImages["pt-BR"];
 }
 
-function getAbsoluteAssetUrl(src) {
-  return new URL(src, SITE_BASE_URL).href;
-}
-
 function updatePreviewImage() {
   const previewImage = getPreviewImage(activeLanguage);
-  const previewImageUrl = getAbsoluteAssetUrl(previewImage.src);
 
   if (mainPreviewImage) {
     mainPreviewImage.src = previewImage.src;
@@ -759,14 +753,6 @@ function updatePreviewImage() {
     mainPreviewImage.width = previewImage.width;
     mainPreviewImage.height = previewImage.height;
   }
-
-  setProperty("og:image", previewImageUrl);
-  setProperty("og:image:secure_url", previewImageUrl);
-  setProperty("og:image:alt", previewImage.alt);
-  setProperty("og:image:width", String(previewImage.width));
-  setProperty("og:image:height", String(previewImage.height));
-  setMeta("twitter:image", previewImageUrl);
-  setMeta("twitter:image:alt", previewImage.alt);
 }
 
 function applyLanguage(language) {
